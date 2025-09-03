023360a80db6e8157f88ff51752acffcae7d73e0
Shivangi10-10 <107495159+Shivangi10-10@users.noreply.github.com>
2025-09-03 12:16:40 +0530

    Analytics update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Analytics } from "@vercel/analytics/next"
 export default function MyApp({ Component, pageProps }) {
+  <Analytics />
   return <Component {...pageProps} />
 }
